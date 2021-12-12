6aab9a24963e1f5e6c2259866e1fc46b22f1a3e3
Zhen Zhang <git@zhen-zhang.com>
2021-12-11 22:37:35 -0800

    Revamp Serve

--- dev/Serve.vue
+++ dev/Serve.vue
@@ -1,13 +1,12 @@
 <template>
-  <div id="app" class="tw-p-5">
-    <h2>Controllers</h2>
-    <button @click="commentMode = !commentMode">
-      切换成
-      <span v-if="commentMode">正常模式</span>
-      <span v-else>评论模式</span>
-    </button>
-    <h2>Tiptap Demo</h2>
-    <div class="tailwind">
+  <div id="app" class="tailwind">
+    <section class="box">
+      <h1 class="tw-text-4xl tw-font-sans">Tiptap Editor Demo</h1>
+      <button @click="commentMode = !commentMode" class="tw-mb-2">
+        切换成
+        <span v-if="commentMode">正常模式</span>
+        <span v-else>评论模式</span>
+      </button>
       <div class="tw-grid tw-grid-cols-2 tw-space-x-1">
         <TiptapCF
           ref="tiptap"
@@ -21,32 +20,46 @@
           @shortcutSubmit="onShortcutSubmit"
         />
         <div>
-          <h2>数据</h2>
-          <pre v-if="content"><code>{{ content }}</code></pre>
-          <h2>文本</h2>
-          <pre v-if="tiptapTextContent"><code>{{ tiptapTextContent }}</code></pre>
+          <div v-if="content">
+            <h2>数据</h2>
+            <pre><code>{{ content }}</code></pre>
+          </div>
+          <div v-if="tiptapTextContent">
+            <h2>文本</h2>
+            <pre><code>{{ tiptapTextContent }}</code></pre>
+          </div>
         </div>
       </div>
-      <hr class="tw-my-2 tw-border-black tw-border-1" />
-      <h2>预览</h2>
+    </section>
+    <section class="box">
+      <h1 class="tw-text-4xl tw-font-sans">Tiptap Viewer Demo</h1>
       <TiptapCF :body="body" :editable="false" />
-      <hr class="tw-my-2 tw-border-black tw-border-1" />
-    </div>
-    <h2>Vditor Demo</h2>
-    <VditorCF ref="vditor" editor-mode="markdown_splitview" @shortcutSubmit="onShortcutSubmit" />
-    <button @click="updateVditorContent">Refresh</button>
-    <div>
-      <h2>Vditor 数据</h2>
-      <pre v-if="vditorContent"><code>{{ vditorContent }}</code></pre>
-    </div>
-    <div>
-      <h2>Vditor 预览</h2>
-      <VditorViewerCF :body="vditorContent" :key="vditorContent" />
-    </div>
-    <div>
-      <h2>Vditor 文本</h2>
-      <pre v-if="vditorTextContent"><code>{{ vditorTextContent }}</code></pre>
-    </div>
+    </section>
+    <section class="box">
+      <h1 class="tw-text-4xl tw-font-sans">Vditor Demo</h1>
+      <div class="tw-grid tw-grid-cols-2 tw-space-x-1">
+        <VditorCF
+          ref="vditor"
+          editor-mode="markdown_splitview"
+          @shortcutSubmit="onShortcutSubmit"
+          :on-editor-change="updateVditorContent"
+        />
+        <div>
+          <div v-if="vditorContent">
+            <h2>数据</h2>
+            <pre><code>{{ vditorContent }}</code></pre>
+          </div>
+          <div v-if="vditorTextContent">
+            <h2>文本</h2>
+            <pre><code>{{ vditorTextContent }}</code></pre>
+          </div>
+          <div v-if="vditorContent">
+            <h2>预览</h2>
+            <VditorViewerCF :body="vditorContent" :key="vditorContent" />
+          </div>
+        </div>
+      </div>
+    </section>
   </div>
 </template>
 
@@ -61,7 +74,7 @@ import VditorViewerCF from '@/lib-components/VditorViewerCF.vue';
 @Component({
   components: { VditorViewerCF, VditorCF, TiptapCF },
 })
-export default class ServeDev extends Vue {
+export default class Serve extends Vue {
   commentMode: boolean = false;
   vditorContent: string = '';
   vditorTextContent: string = '';
@@ -133,3 +146,11 @@ export default class ServeDev extends Vue {
   }
 }
 </script>
+
+<style>
+.box {
+  margin: 5px;
+  padding: 10px;
+  border: 1px solid lightgray;
+}
+</style>
--- dev/serve.ts
+++ dev/serve.ts
@@ -1,5 +1,5 @@
 import Vue, { VNode } from 'vue';
-import Dev from './serve.vue';
+import Serve from './Serve.vue';
 // To register individual components where they are used (serve.vue) instead of using the
 // library as a whole, comment/remove this import and it's corresponding "Vue.use" call
 import ChafanVueEditors from '@/entry.esm';
@@ -8,5 +8,5 @@ Vue.use(ChafanVueEditors);
 Vue.config.productionTip = false;
 
 new Vue({
-  render: (h): VNode => h(Dev),
+  render: (h): VNode => h(Serve),
 }).$mount('#app');
